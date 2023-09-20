857a605e8b16127aaa468b3a25925926243b1aff
Tymur Bondar <tymurbondar@outlook.com>
2023-09-20 11:17:50 -0400

    all the core delivs working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-let ramenMenuDiv = document.getElementById("ramen-menu");
+const ramenMenuDiv = document.getElementById("ramen-menu");
+const newRamenForm = document.getElementById("new-ramen");
+let newId = 1;
 // write your code here
 // new code
 //core challenge 1
@@ -15,6 +17,7 @@ function displayDetails(ramen) {
     restaurant.textContent = ramen.restaurant;
     rating.textContent = ramen.rating;
     comment.textContent = ramen.comment;
+    newId++;
 }
 
 function displayPhoto(ramen) {
@@ -36,3 +39,17 @@ fetch("http://localhost:3000/ramens")
             displayPhoto(ramen);
         });
     })
+
+newRamenForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newRamen = {
+        id: newId,
+        name: e.target.name.value,
+        restaurant: e.target.restaurant.value,
+        image: e.target.image.value,
+        rating: e.target.rating.value,
+        comment: e.target.comment.value
+    }
+    displayPhoto(newRamen);
+    displayDetails(newRamen);
+})
